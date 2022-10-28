de3c3de7d863899505f05c92897fd86d529cdd41
unknown <tavoywalls@gmail.com>
2022-10-28 15:21:05 -0400

    JWT Auth

--- frontend/src/places/NewCommentForm.js
+++ frontend/src/places/NewCommentForm.js
@@ -16,7 +16,7 @@ function NewCommentForm({ place, onSubmit }) {
         const fetchData = async () => {
             const response = await fetch(`http://localhost:5000/users`)
             const users = await response.json()
-            setComment({ ...comment, authorId: users[0]?.userId})
+            setComment({ ...comment, authorId: users[0]?.userId })
             setAuthors(users)
         }
         fetchData()
@@ -35,6 +35,12 @@ function NewCommentForm({ place, onSubmit }) {
             rant: false,
             authorId: authors[0]?.userId
         })
+    }
+
+    const { currentUser } = useContext(CurrentUser)
+
+    if (!currentUser) {
+        return <p>You must be logged in to leave a rant or rave.</p>
     }
 
     return (
